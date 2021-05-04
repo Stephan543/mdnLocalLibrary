025aea6304b2c83e79f529eb41d1ab492360ad2f
Stephan Iskander <stephan.iskander@gmail.com>
2021-05-04 11:07:00 -0400

    Fixing DateTime for author lifespan using Luxon

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 var AuthorSchema = new Schema(
     {
@@ -22,6 +23,20 @@ AuthorSchema
 .virtual('lifespan')
 .get(function () {
     return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+});
+
+// Virtual for author's formatted date of birth
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+    return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+// Virtual for author's formatted lifespan
+AuthorSchema
+.virtual('date_of_death_formatted')
+.get(function () {
+    return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
 });
 
 // Virtual for author's url
