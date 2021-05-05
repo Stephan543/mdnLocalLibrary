1b77743327f2e618ad45ab4027db7049a1c659cf
Stephan Iskander <stephan.iskander@gmail.com>
2021-05-05 15:52:56 -0400

    adding in the author detail page with book associations

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 var Author = require('../models/author'); // upstream dir
+var async = require('async');
+var Book = require('../models/book');
 
 // Display list of all Authors.
 exports.author_list = function(req, res, next){
@@ -12,8 +14,28 @@ exports.author_list = function(req, res, next){
 };
 
 // Display detail page for specific Author
-exports.author_detail = function(req, res){
-    res.send("NOT IMPLEMENTED: Author detail: " + req.params.id);
+exports.author_detail = function(req, res, next){
+    
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id)
+            .exec(callback)
+        },
+        author_books: function(callback){
+            Book.find({ 'author': req.params.id}, 'title summary')
+            .exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); } // Error in API usage
+        if (results.author==null) { 
+            // no result
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);            
+        }
+        // Successful so...
+        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
+    });
 };
 
 // Display Author create form on GET
