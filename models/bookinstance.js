a8eac3e8bf711a5d911984b290e677a0939fbef3
Stephan Iskander <stephan.iskander@gmail.com>
2021-05-05 18:32:21 -0400

    really wierd bug with live server and url, issue fixed after reloading and saving the file

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -16,7 +16,7 @@ var BookInstanceSchema = new Schema(
 BookInstanceSchema
 .virtual('url')
 .get(function (){
-    return 'catalog/bookinstance/' + this._id;
+    return '/catalog/bookinstance/' + this._id;
 });
 
 // Virtual for due back date formatted
